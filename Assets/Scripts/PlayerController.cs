224ff589f6ee195e13c1cea9c969cd1af05e51ad
SakonwatYim <sakonwat.yimnapoe@gmail.com>
2026-03-06 10:57:55 +0700

    finish

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,15 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     public float gravityMultiplier = 1f;
     public bool gameOver = false;
+
+    public ParticleSystem fxDirt;
+    public GameObject fxExplosionPrefab;
+
+    public Animator animator;
+
+    public AudioClip sfxCrash;
+    public AudioClip sfxJump;
+
     private Rigidbody rb;
     private InputAction jumpAction;
     // 5.8 add audio source variable to play crash sound
@@ -15,6 +24,8 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
+        animator = GetComponent<Animator>();
+
         rb = GetComponent<Rigidbody>();
         jumpAction = InputSystem.actions.FindAction("Jump");
 
@@ -30,6 +41,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         Physics.gravity *= gravityMultiplier;
+
+        animator.SetFloat("Speed_f", 1.0f);
     }
 
     // Update is called once per frame
@@ -44,6 +57,9 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
             isOnGround = false;
+            animator.SetTrigger("Jump trig");
+            fxDirt.Stop();
+            audioSource.PlayOneShot(sfxJump);
         }
     }
 
@@ -52,11 +68,16 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            fxDirt.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            animator.SetBool("Death_b" , true);
+            animator.SetInteger("DeathType_int", 1);
+            Instantiate(fxExplosionPrefab,transform.position,Quaternion.identity);
+            audioSource.PlayOneShot(sfxCrash);
         }
     }
 }
